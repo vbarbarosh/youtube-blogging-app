55119fc6b44bf73a251522a6aad723b35d674e35
Vladimir Barbaros <vladimir.b@bannernow.com>
2023-06-11 20:24:43 +0300

    api_articles_fetch: fix

--- resources/js/helpers/api/api_articles_fetch.js
+++ resources/js/helpers/api/api_articles_fetch.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function api_articles_fetch(article)
 {
-    return axios.get(`/api/v1/articles/${article.uid}`).then(v => v.data);
+    return axios.get(`/api/v1/articles/${article.uid}.json`).then(v => v.data);
 }
 
 export default api_articles_fetch;
