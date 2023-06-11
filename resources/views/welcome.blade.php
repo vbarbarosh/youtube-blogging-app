63ed718dcf279c2be7d098985d88ba5ca33da649
Vladimir Barbaros <vladimir.b@bannernow.com>
2023-06-11 20:52:49 +0300

    add smcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="https://unpkg.com/@vbarbarosh/smcss@0.8.4/dist/sm.css" rel="stylesheet" crossorigin="anonymous">
 </head>
 <body>
     <div id="app"></div>
